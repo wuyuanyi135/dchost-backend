58a150a4b38237709bf3db4ad5839f349b572f88
wuyuanyi135 <wuyuanyi135@gmail.com>
2016-01-15 03:47:31 -0500

    fix `find` not found

--- index.js
+++ index.js
@@ -4,10 +4,15 @@ var app = feathers();
 var tempSwitchModule = {
   path: '/temp/switch',
   tempSwitchService: {
+    find: function find(param,callback) {
+      callback (null, {
+        text: "hihi"
+      })
+    },
     get: function get(id, params, callback) {
       callback (null,{
         id: id,
-        text: true
+        text: "true"
       })
     }
   }
